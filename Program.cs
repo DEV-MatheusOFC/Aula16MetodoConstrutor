6ca6767f25e6aeee56fa5b568c7a9b25dcaae0f7
DEV-MatheusOFC <matheusnunesoliveira198@gmail.com>
2020-06-18 10:50:25 -0300

    Aula16 Finalizada

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Aula_16___Metodo_Construtor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Produto celular = new Produto();
+
+            Produto tenis = new Produto(588);
+            System.Console.WriteLine(tenis.Codigo);            
+            Produto carro = new Produto(549, "Peugeot", "carro vrum vrum", 1);
+            System.Console.WriteLine($"\n{carro.Codigo}\n{carro.Nome}\n{carro.Descricao}\n{carro.Estoque}");
+        }
+    }
+}
